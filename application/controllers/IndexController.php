56633a4c21e59db196a185523a54cb11f1f28b45
Eric Kansa <ekansa@alexandriaarchive.org>
2012-10-29 17:10:20 -0700

    Start revisions to OC homepage
    Not working yet, but will...

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -8,58 +8,58 @@ class indexController extends Zend_Controller_Action
     public function indexAction()
     {
 	
-	$host = OpenContext_OCConfig::get_host_config();
-	if(substr_count($_SERVER['HTTP_HOST'], "www.")>0){
-	    $reDirectURI = $host;
-	    header('Location: '.$reDirectURI);
-	    exit;
-	}
-	
-	
-	OpenContext_SocialTracking::update_referring_link('home', $this->_request->getRequestUri(), @$_SERVER['HTTP_USER_AGENT'], @$_SERVER['HTTP_REFERER']);
-	
-	$useragent = @$_SERVER['HTTP_USER_AGENT'];
-    	if(strstr($useragent,"MSIE 6")||strstr($useragent,"MSIE 7")||strstr($useragent,"MSIE")||strstr($useragent,"Android")){
-	    return $this->render('ieindex'); // re-render the estimate form
-	}
+		  $host = OpenContext_OCConfig::get_host_config();
+		  if(substr_count($_SERVER['HTTP_HOST'], "www.")>0){
+				$reDirectURI = $host;
+				header('Location: '.$reDirectURI);
+				exit;
+		  }
+		  
+		  
+		  OpenContext_SocialTracking::update_referring_link('home', $this->_request->getRequestUri(), @$_SERVER['HTTP_USER_AGENT'], @$_SERVER['HTTP_REFERER']);
+		  
+		  $useragent = @$_SERVER['HTTP_USER_AGENT'];
+			  if(strstr($useragent,"MSIE 6")||strstr($useragent,"MSIE 7")||strstr($useragent,"MSIE")){
+				return $this->render('ieindex'); // re-render the estimate form
+		  }
 	
     }
     
     public function robotsAction() {
-	$this->_helper->viewRenderer->setNoRender();
-	$host = OpenContext_OCConfig::get_host_config();
-	
-	
-	
-	
-	
-	$robots = "
-	User-agent:*\r\n
-	Crawl-Delay:0.5\r\n
-	Disallow:/cgi-bin/\r\n
-	Disallow:/tmp/\r\n
-	Disallow:/sets/\r\n
-	Disallow:/lightbox/\r\n
-	Disallow:/search/\r\n
-	Disallow: /*&\r\n
-	Sitemap: http://opencontext.org/data/siteMap-1.xml \r\n
-	Sitemap: http://opencontext.org/data/siteMap-2.xml \r\n
-	Sitemap: http://opencontext.org/data/siteMap-3.xml \r\n
-	Sitemap: http://opencontext.org/data/siteMap-4.xml \r\n
-	Sitemap: http://opencontext.org/data/siteMap-5.xml \r\n
-	Sitemap: http://opencontext.org/data/siteMap-6.xml \r\n
-	Sitemap: http://opencontext.org/data/siteMap-7.xml \r\n
-	Sitemap: http://opencontext.org/data/siteMap-8.xml \r\n
-	Sitemap: http://opencontext.org/data/siteMap-9.xml \r\n
-	Sitemap: http://opencontext.org/data/siteMap-10.xml \r\n
-	Sitemap: http://opencontext.org/data/siteMap-11.xml \r\n
-	Sitemap: http://opencontext.org/data/siteMap-12.xml \r\n
-	Sitemap: http://opencontext.org/data/siteMap-13.xml \r\n
-	Sitemap: http://opencontext.org/data/siteMap-14.xml \r\n
-	";
-	
-	header("Content-Type: text/plain");
-	echo $robots;
+		  $this->_helper->viewRenderer->setNoRender();
+		  $host = OpenContext_OCConfig::get_host_config();
+		  
+		  
+		  
+		  
+		  
+		  $robots = "
+		  User-agent:*\r\n
+		  Crawl-Delay:0.5\r\n
+		  Disallow:/cgi-bin/\r\n
+		  Disallow:/tmp/\r\n
+		  Disallow:/sets/\r\n
+		  Disallow:/lightbox/\r\n
+		  Disallow:/search/\r\n
+		  Disallow: /*&\r\n
+		  Sitemap: http://opencontext.org/data/siteMap-1.xml \r\n
+		  Sitemap: http://opencontext.org/data/siteMap-2.xml \r\n
+		  Sitemap: http://opencontext.org/data/siteMap-3.xml \r\n
+		  Sitemap: http://opencontext.org/data/siteMap-4.xml \r\n
+		  Sitemap: http://opencontext.org/data/siteMap-5.xml \r\n
+		  Sitemap: http://opencontext.org/data/siteMap-6.xml \r\n
+		  Sitemap: http://opencontext.org/data/siteMap-7.xml \r\n
+		  Sitemap: http://opencontext.org/data/siteMap-8.xml \r\n
+		  Sitemap: http://opencontext.org/data/siteMap-9.xml \r\n
+		  Sitemap: http://opencontext.org/data/siteMap-10.xml \r\n
+		  Sitemap: http://opencontext.org/data/siteMap-11.xml \r\n
+		  Sitemap: http://opencontext.org/data/siteMap-12.xml \r\n
+		  Sitemap: http://opencontext.org/data/siteMap-13.xml \r\n
+		  Sitemap: http://opencontext.org/data/siteMap-14.xml \r\n
+		  ";
+		  
+		  header("Content-Type: text/plain");
+		  echo $robots;
 		
     }
     
