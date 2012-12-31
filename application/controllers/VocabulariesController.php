ab2a7dc3e17dbdcda44b86a5f4db79b45fe07f9f
Eric Kansa <ekansa@alexandriaarchive.org>
2012-12-30 17:32:26 -0800

    Parse and display of OWL files
    Getting to the point where we can view them.

--- application/controllers/VocabulariesController.php
+++ application/controllers/VocabulariesController.php
@@ -14,20 +14,42 @@ class VocabulariesController extends Zend_Controller_Action
     
     public function viewAction() {
 		
-		
+		  $host = OpenContext_OCConfig::get_host_config();
         $this->view->ok = true;
         OpenContext_SocialTracking::update_referring_link('vocabs', $this->_request->getRequestUri(), @$_SERVER['HTTP_USER_AGENT'], @$_SERVER['HTTP_REFERER']);        
         
 		  $vocab =  $this->_request->getParam('vocab');
 		  $concept =  $this->_request->getParam('concept');
-		  
-		  $this->_helper->viewRenderer->setNoRender();
 	 
 		  $OWL = new OWL;
-		  $OWL->getOntology($vocab, $concept);
-		  header('Content-Type: application/json; charset=utf8');
-		  $output = Zend_Json::encode($OWL->owlArray);
-		  echo $output;
+		  $OWL->requestURI = $host.$this->_request->getRequestUri();
+		  
+		  if($OWL->getOntology($vocab, $concept)){
+				if(isset($_GET["format"])){
+					 $this->_helper->viewRenderer->setNoRender();
+					 header('Content-Type: application/json; charset=utf8');
+					 $output = Zend_Json::encode($OWL->owlArray);
+					 echo $output;
+				}
+				elseif($concept){
+					 if($OWL->conceptFound){
+						  $this->view->requestURI = $host.$this->_request->getRequestUri();
+						  $this->view->OWL = $OWL;
+						  return $this->render('view-part'); // re-render the login form
+					 }
+					 else{
+						  $this->view->requestURI = $host.$this->_request->getRequestUri();
+						  return $this->render('404error'); // concept not found in ontology
+					 }
+				}
+				else{
+					 $this->view->OWL = $OWL;
+				}
+		  }
+		  else{
+				$this->view->requestURI = $host.$this->_request->getRequestUri();
+				return $this->render('404error'); // ontology not found
+		  }
     }
     
     
--- application/models/OWL.php
+++ application/models/OWL.php
@@ -5,22 +5,31 @@ Manages, parses, and uses OWL ontologies
 */
 
 class OWL {
- 
+
 public $vocab; //name of the OWL ontology from the URL (slug)
+public $vocabLabel; //label for the OWL ontology
+public $vocabURI; //URI for the current vocabulary
+public $vocabStatus; //URI for the vocabularie's peer review status (if given)
 public $concept; //name-identifier (slug) for a concept referenced in the request URL
-public $hashConcept; //fragment identified concept
+public $conceptFound; //boolean, if the requested concept identifier was found in the ontology
+public $conceptLabel; //label for the active concept, if found.
+public $conceptType; //false if not found, or class or property
 
 public $OWLfile; //filename for the OWL ontology
 public $xml; //simple xml of the ontology
 public $owlArray; //array of the full OWL ontology
 
-
 public $created; //when was the ontology first created
 public $updated; //when was the ontology last updated
 
+public $requestURI; //current request URI
 public $db;
 
 const ontologyDirectory = "C:\\GitHub\\oc-ontologies\\vocabularies\\";
+const labelAbbrevIRI = "rdfs:label";
+const commentAbbrevIRI = "rdfs:comment";
+const objectPropRange = "ObjectPropertyRange";
+const vocabReviewIRI = "bibo:status";
 
     function getOntology($vocab, $concept = false){
         
@@ -30,7 +39,12 @@ const ontologyDirectory = "C:\\GitHub\\oc-ontologies\\vocabularies\\";
 		  $this->setUTFconnection($db);    
         
         $this->vocab = false;
+		  $this->vocabLabel = false;
+		  $this->vocabURI = false;
+		  $this->vocabStatus = false;
 		  $this->concept = false;
+		  $this->conceptLabel = false;
+		  $this->conceptType = false;
 		  $this->owlArray = false;
 		  
 		  if($concept != false){
@@ -53,7 +67,7 @@ const ontologyDirectory = "C:\\GitHub\\oc-ontologies\\vocabularies\\";
             $this->created = $result[0]["created"];
             $this->updated = $result[0]["updated"];
             $this->vocab = $vocab;
-            
+				
             $sFilename = self::ontologyDirectory.$this->OWLfile;
             @$xmlString = $this->loadFile($sFilename);
             if($xmlString != false){
@@ -63,9 +77,11 @@ const ontologyDirectory = "C:\\GitHub\\oc-ontologies\\vocabularies\\";
 						  $this->OWLtoArray();
                 }
             }
-            
+            return true;
         }
-        
+        else{
+				return false;
+		  }
     }
     
 	 
@@ -78,6 +94,39 @@ const ontologyDirectory = "C:\\GitHub\\oc-ontologies\\vocabularies\\";
 					 @$xml->registerXPathNamespace($prefix, $uri);
 				}
 				$owlArray = array();
+				
+				$ontologyAnnotations = array();
+				foreach($xml->xpath("/owl:Ontology/@ontologyIRI") as $xpathResult){
+					 $this->vocabURI = (string)$xpathResult;
+				}
+				
+				foreach($xml->xpath("/owl:Ontology/owl:Annotation") as $assertion){
+					 $nameSpaceArray = $this->nameSpaces();
+					 foreach($nameSpaceArray as $prefix => $uri){
+						  @$assertion->registerXPathNamespace($prefix, $uri);
+					 }
+					 $prop = false;
+					 $propVal = false;
+					 foreach($assertion->xpath("owl:AnnotationProperty/@abbreviatedIRI") as $xpathResult){
+						  $prop = (string)$xpathResult;
+					 }
+					 foreach($assertion->xpath("owl:Literal") as $xpathResult){
+						  $propVal = (string)$xpathResult;
+					 }
+					 
+					 if($prop == self::labelAbbrevIRI && $propVal != false){
+					   $this->vocabLabel = $propVal;
+					 }
+					 if($prop == self::vocabReviewIRI && $propVal != false){
+					   $this->vocabStatus = $propVal;
+					 }
+					 
+					 if($prop != false && $propVal != false){
+						  $ontologyAnnotations[] = array($prop => $propVal);
+					 }
+				}
+				$owlArray["ontology"] = $ontologyAnnotations;
+				
 				
 				$classes = array();
 				foreach($xml->xpath("//owl:Declaration/owl:Class/@IRI") as $xpathResult){
@@ -92,7 +141,10 @@ const ontologyDirectory = "C:\\GitHub\\oc-ontologies\\vocabularies\\";
             foreach($xml->xpath("//owl:SubClassOf/owl:Class[2]/@IRI") as $xpathResult){
 					$parent = (string)$xpathResult;
                if(!array_key_exists($parent, $classes)){
-                $externalParents[] = $parent;
+						  $externalParents[] = $parent;
+						  if(!array_key_exists($parent, $classes)){
+								$classes[$parent] = array();
+						  }
                }
                $parentIsChild = false;
                foreach($xml->xpath("//owl:SubClassOf/owl:Class[1][@IRI = '$parent']") as $xpathResultB){
@@ -100,6 +152,9 @@ const ontologyDirectory = "C:\\GitHub\\oc-ontologies\\vocabularies\\";
                }
                if(!$parentIsChild){
                     $rootParents[] = $parent;
+						  if(!array_key_exists($parent, $classes)){
+								$classes[$parent] = array();
+						  }
                }
 				}
             $owlArray["externalParents"] = $externalParents;
@@ -112,27 +167,46 @@ const ontologyDirectory = "C:\\GitHub\\oc-ontologies\\vocabularies\\";
             }
             $owlArray["hierachy"] = $hierarchy;
             
-            //get annotations on classes
-            /*
-            foreach($classes as $classKey => $classAnnotations){
-                foreach($xml->xpath("//owl:AnnotationAssertion/owl:IRI[text() = '$classKey']") as $assertionIRI){
-                    $annotations = array();
+				$properties = array();
+				foreach($xml->xpath("//owl:Declaration/owl:ObjectProperty/@IRI") as $xpathResult){
+					$property = (string)$xpathResult;
+               $properties[$property] = array();
+				}
+				
+				$propertyAnnotations = array();
+            foreach($properties as $propKey => $propArray){
+                foreach($xml->xpath("//owl:AnnotationAssertion[owl:IRI[text() = '$propKey']]") as $assertionIRI){
                     $nameSpaceArray = $this->nameSpaces();
                     foreach($nameSpaceArray as $prefix => $uri){
                         @$assertionIRI->registerXPathNamespace($prefix, $uri);
                     }
-                    foreach($assertionIRI->xpath("./owl:AnnotationProperty/@abbreviatedIRI") as $xpathResult){
+                    foreach($assertionIRI->xpath("owl:AnnotationProperty/@abbreviatedIRI") as $xpathResult){
                         $prop = (string)$xpathResult;
                     }
-                    foreach($assertionIRI->xpath("./owl:Literal") as $xpathResult){
+                    foreach($assertionIRI->xpath("owl:Literal") as $xpathResult){
                         $propVal = (string)$xpathResult;
                     }
-                    $classAnnotations[] = array($prop => $propVal);
+                    $propertyAnnotations[$propKey][] = array($prop => $propVal);
                 }
+					 
+					 foreach($xml->xpath("//owl:ObjectPropertyRange[owl:ObjectProperty[@IRI = '$propKey']]") as $range){
+						  $nameSpaceArray = $this->nameSpaces();
+                    foreach($nameSpaceArray as $prefix => $uri){
+                        @$range->registerXPathNamespace($prefix, $uri);
+                    }
+						  foreach($range->xpath("owl:Class/@IRI") as $xpathResult){
+                        $rangeIRI = (string)$xpathResult;
+								
+								if(!array_key_exists($rangeIRI, $classes)){
+									 $classes[$rangeIRI] = array();
+								}
+								
+								$propertyAnnotations[$propKey][] = array(self::objectPropRange => $rangeIRI);
+                    }
+					 }
             }
-            */
-            
-            $classAnnotations = array();
+				
+				$classAnnotations = array();
             foreach($classes as $classKey => $classArray){
                 foreach($xml->xpath("//owl:AnnotationAssertion[owl:IRI[text() = '$classKey']]") as $assertionIRI){
                     $nameSpaceArray = $this->nameSpaces();
@@ -148,12 +222,53 @@ const ontologyDirectory = "C:\\GitHub\\oc-ontologies\\vocabularies\\";
                     $classAnnotations[$classKey][] = array($prop => $propVal);
                 }
             }
+				
+				$owlArray["classes"] = $classAnnotations;
+				$owlArray["properties"] = $propertyAnnotations;
             
-            $owlArray["classes"] = $classAnnotations;
-            
-            
+				if($this->concept){
+					 //checks to see if the requested concept actually exists in the ontology
+					 $this->conceptFound = false;
+					 $slashConcept = "/".$this->concept;
+					 if(array_key_exists($this->concept, $classAnnotations)){
+						  $this->conceptFound = true;
+						  $this->conceptType = "class";
+						  $this->activeConceptLabel($this->concept, $classAnnotations);
+					 }
+					 elseif(array_key_exists($this->concept, $propertyAnnotations)){
+						  $this->conceptFound = true;
+						  $this->conceptType = "property";
+						  $this->activeConceptLabel($this->concept, $propertyAnnotations);
+					 }
+					 elseif(array_key_exists($slashConcept, $classAnnotations)){
+						  $this->concept = $slashConcept;
+						  $this->conceptFound = true;
+						  $this->conceptType = "class";
+						  $this->activeConceptLabel($slashConcept, $classAnnotations);
+					 }
+					 elseif(array_key_exists($slashConcept, $propertyAnnotations)){
+						  $this->concept = $slashConcept;
+						  $this->conceptFound = true;
+						  $this->conceptType = "property";
+						  $this->activeConceptLabel($slashConcept, $propertyAnnotations);
+					 }
+					 
+					 if(!$this->vocabURI){
+						  //fallback way of getting the vocabulary URI
+						  $this->getVocabularyURI();
+					 }
+				}
+				
 				$this->owlArray = $owlArray;
 		  }
+	 }
+	 
+	 
+	 function getVocabularyURI(){
+		  $conceptLen = strlen($this->concept);
+		  $requestLen = strlen($this->requestURI);
+		  $vocabularyURI = substr($this->requestURI, 0, ($requestLen - $conceptLen));
+		  $this->vocabURI = $vocabularyURI;
 	 }
 	 
 	 
@@ -187,8 +302,124 @@ const ontologyDirectory = "C:\\GitHub\\oc-ontologies\\vocabularies\\";
         return $output;
     }
     
+    //get the label for the active concept
+	 function activeConceptLabel($actConcept, $conceptArray){
+		  if(!$this->conceptLabel){
+				$actConArray = $conceptArray[$actConcept];
+				foreach($actConArray as $annoationArray){
+					 if(array_key_exists(self::labelAbbrevIRI, $annoationArray)){
+						  $this->conceptLabel = $annoationArray[self::labelAbbrevIRI];
+					 }
+				}
+		  }
+	 }
+	 
+	 
+	 function IRIgetLabel($actIRI, $actArray){
+		  $output = false;
+		  if(array_key_exists($actIRI, $actArray)){
+				$actConArray =  $actArray[$actIRI];
+				foreach($actConArray as $annoationArray){
+					 if(array_key_exists(self::labelAbbrevIRI, $annoationArray)){
+						  $output = $annoationArray[self::labelAbbrevIRI];
+					 }
+				}
+		  }
+		  return $output;
+	 }
+	 
+	 function IRIgetComment($actIRI, $actArray){
+		  $output = false;
+		  if(array_key_exists($actIRI, $actArray)){
+				$actConArray =  $actArray[$actIRI];
+				foreach($actConArray as $annoationArray){
+					 if(array_key_exists(self::commentAbbrevIRI, $annoationArray)){
+						  $output = $annoationArray[self::commentAbbrevIRI];
+					 }
+				}
+		  }
+		  return $output;
+	 }
+	 
+	 function IRIgetObjectPropRange($actIRI){
+		  $output = false;
+		  $owlArray = $this->owlArray;
+		  if(array_key_exists($actIRI, $owlArray["properties"])){
+				$actConArray =  $owlArray["properties"][$actIRI];
+				foreach($actConArray as $annoationArray){
+					 if(array_key_exists(self::objectPropRange, $annoationArray)){
+						  $output = $annoationArray[self::objectPropRange];
+					 }
+				}
+		  }
+		  return $output;
+	 }
+	 
+	 //convert an IRI to a link
+	 function IRItoURL($actIRI){
+		  if(stristr($actIRI, "http://")){
+				return $actIRI;
+		  }
+		  else{
+				return $this->vocabURI.$actIRI;
+		  }
+	 }
     
-    
+	 
+	 function getStatusLevel(){
+		  $output = 0;
+		  if($this->vocabStatus != false){
+				$output = 2;
+				if(strstr($this->vocabStatus, "http://opencontext.org/about/publishing/#edit-stars-")){
+					 $output = str_replace("http://opencontext.org/about/publishing/#edit-stars-", "", $this->vocabStatus);
+				}
+		  }
+		  return $output;
+	 }
+	 
+	 function reviewStarHTML(){
+		  $output = "Not reviewed";
+		  $level = $this->getStatusLevel();
+		  if($level == 1){
+				$output = "&#9733;&#9734;&#9734;&#9734;&#9734;";
+		  }
+		  elseif($level == 2){
+				$output = "&#9733;&#9733;&#9734;&#9734;&#9734;";
+		  }
+		  elseif($level == 3){
+				$output = "&#9733;&#9733;&#9733;&#9734;&#9734;";
+		  }
+		  elseif($level == 4){
+				$output = "&#9733;&#9733;&#9733;&#9733;&#9734;";
+		  }
+		  elseif($level == 5){
+				$output = "&#9733;&#9733;&#9733;&#9733;&#9733;";
+		  }
+		  return $output;
+	 }
+	 
+	 function reviewLabelHTML(){
+		  $output = "";
+		  $level = $this->getStatusLevel();
+		  if($level == 1){
+				$output = "Demonstration";
+		  }
+		  elseif($level == 2){
+				$output = "Minimal editorial acceptance";
+		  }
+		  elseif($level == 3){
+				$output = "Managing editor reviewed";
+		  }
+		  elseif($level == 4){
+				$output = "Editorial board reviewed";
+		  }
+		  elseif($level == 5){
+				$output = "Peer-reviewed";
+		  }
+		  return $output;
+	 }
+	 
+	 
 	 function nameSpaces(){
 		  $nameSpaceArray = array(
 		  "owl"=> "http://www.w3.org/2002/07/owl#",
