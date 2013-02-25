184232fb4d76e878bd450ab43dca68cec36e24fa
Eric Kansa <ekansa@alexandriaarchive.org>
2013-02-24 22:13:27 -0800

    Orcid ID

--- application/controllers/PersonsController.php
+++ application/controllers/PersonsController.php
@@ -144,7 +144,7 @@ class personsController extends Zend_Controller_Action
 				$this->_helper->viewRenderer->setNoRender();
 				header('Content-type: application/xml', true);
 				
-				//$person->archaeoML = $person->addLinkedData($person->archaeoML, "http://xmlns.com/foaf/0.1/isPrimaryTopicOf", "http://orcid.org/0000-0001-7920-5321");
+				//$person->archaeoML = $person->addLinkedData($person->archaeoML, "http://xmlns.com/foaf/0.1/isPrimaryTopicOf", "http://orcid.org/0000-0002-9723-4561");
 				//OpenContext_NewDocs::personAdd($person->archaeoML);
 				
 				$XML = simplexml_load_string($person->archaeoML);
