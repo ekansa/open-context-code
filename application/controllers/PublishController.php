0a1ca393c26d51dc04068bb986cadd4500b02bee
Eric Kansa <ekansa@alexandriaarchive.org>
2013-05-24 18:29:51 -0700

    New JSON-LD table class and publishing
    Part of code refactoring for downloadable tables. Tables will be
    documented with JSON-LD encoded metadata. These commit has new class and
    publisher controller to publish new table metadata record.

--- application/controllers/PublishController.php
+++ application/controllers/PublishController.php
@@ -20,6 +20,9 @@ class publishController extends Zend_Controller_Action
     //const xmlRoot = "http://ux.opencontext.org/xml/oc_xmlgen/"; 
     
     public function docAddAction() {
+		  
+		  mb_internal_encoding( 'UTF-8' );
+		  
 		  $this->_helper->viewRenderer->setNoRender();	
         $type = $_REQUEST["type"];
         $id = $_REQUEST["id"];
@@ -114,7 +117,7 @@ class publishController extends Zend_Controller_Action
     
     public function itemPublishAction() {
 		  $this->_helper->viewRenderer->setNoRender();	
-		  
+		  mb_internal_encoding( 'UTF-8' );
 		  
 		  //id of the document to processed
 		  if(!isset($_REQUEST["itemUUID"])){
@@ -231,273 +234,42 @@ class publishController extends Zend_Controller_Action
 					 }
 				}
 		  }
+		  
 		  header('Content-Type: application/json; charset=utf8');
         echo Zend_Json::encode($output) ;
+        
+		  //echo $data->archaeoML;
     }
 	 
 
     
     
-    public function indexOmekaAction() {
-	$this->_helper->viewRenderer->setNoRender();	
-	
-	$jsonString = false;
-	$jsonObject = false;
-	$OmekaURL = false;
-	$errorArray = array();
-	
-	$omeka = new Omeka;
-	
-	
-	if(isset($_REQUEST["url"])){
-	    $OmekaURL = $_REQUEST["url"];
-	}
-	else{
-	    $errorCode = 400;
-	    $headerString = "HTTP/1.1 400 Bad Request";
-	    $errorArray[] = array("error" => $errorCode,
-			    "errorMessage" => "Please provide the URL to the Omeka item in the 'url' parameter.");
-	}
-	
-	
-	if(isset($_REQUEST["json"])){
-	    $jsonString = $_REQUEST["json"];
-	    $jsonString = stripslashes($jsonString);
-	    $jsonObject = Zend_Json::decode($jsonString);
-	    if($jsonObject == false){
-		$errorCode = 400;
-		$headerString = "HTTP/1.1 400 Bad Request";
-		$errorArray[] = array("error" => $errorCode,
-				"errorMessage" => "Could not parse JSON POSTed in the parameter 'json'.",
-				"POSTed_json" => $jsonString);
-	    }
-	}
-	else{
-	    $errorCode = 400;
-	    $headerString = "HTTP/1.1 400 Bad Request";
-	    $errorArray[] = array("error" => $errorCode,
-			    "errorMessage" => "Please POST Omeka-JSON in the parameter 'json'.");
-	}
-	
-	if($jsonObject != false && $jsonString != false && $OmekaURL != false){
-	    
-	    $omeka->json = $jsonString;
-	    $omeka->itemURI = $OmekaURL;
-	    $omeka->label = $OmekaURL;
-	    $omeka->baseURL = $OmekaURL;
-	    $omeka->add_update_doc($OmekaURL);
-
-	    $errorArray = false;
-	    $headerString = "HTTP/1.1 200 OK";
-	    $cache_id = md5($OmekaURL);
-	    $frontendOptions = array(
-		    'lifetime' => 72000, // cache lifetime, measured in seconds, 7200 = 2 hours
-		    'automatic_serialization' => true
-	    );
-		    
-	    $backendOptions = array(
-		'cache_dir' => './omeka_cache/' // Directory where to put the cache files
-	    );
-		    
-	    $cache = Zend_Cache::factory('Core',
-				 'File',
-				 $frontendOptions,
-				 $backendOptions);
-	    
-	    
-	    
-	    //$cache->clean(Zend_Cache::CLEANING_MODE_OLD); // clean old cache records
-	    if(!$cache_result = $cache->load($cache_id)) {
-		$cache->save($jsonString, $cache_id); //save result to the cache
-	    }
-	    
-	    
-	    
-	    
-	}
-	
-	
-	$output["errors"] = $errorArray;
-	$output["url"] = $OmekaURL;
-	$output["POSTed_json"] = $jsonString;
-	
-	header($headerString);
-	echo Zend_Json::encode($output);
-    }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    /*
-    Sample Bad XML:
-    
-    <opencont_penimporter>
-    <!-- Table w_space -->
-    <w_space>
-        <Bad_UUID>9CD6A5A1-CB3E-4756-0344-AC7F9054416A</Bad_UUID>
-        <Bad_UUID_name>SDI-16774</Bad_UUID_name>
-        <space_label>SDAC 203</space_label>
-        <uuid>CC27BA46-6B7A-4870-10CF-53F00F3DE36B</uuid>
-    </w_space> 
-     
-     
-    */
-    
-    
-    /*
-    //is deletes items either, and updates parent items of deleted items to not contain links to deleted children
-    //it saves deleted items in a table so the deletes can be undone and / or archived
-    public function docDeleteAction(){
-	
-	$this->_helper->viewRenderer->setNoRender();	
-	$host = OpenContext_OCConfig::get_host_config();
+	 public function tablePublishAction() {
+		  $this->_helper->viewRenderer->setNoRender();	
+		  mb_internal_encoding( 'UTF-8' );
+		  
+		  if(isset($_REQUEST["json"])){
+				$json = $_REQUEST["json"];
+				$tabObj = new ExportTable;
+				$ok = $tabObj->createUpdate($json);
+				unset($json);
+				if($ok){
+					 $output = $tabObj->metadata;
+				}
+				else{
+					 $output = array("error" => "Bad JSON data");
+				}
+		  }
+		  else{
+				$output = array("error" => "no data");
+		  }
+		  
+		  header('Content-Type: application/json; charset=utf8');
+        echo Zend_Json::encode($output) ;
         
-	$auth = $_REQUEST["auth"];
-	
-	if($auth != "zapper1"){
-	    echo "not authorized";
-	    break;
-	}
-	else{
-	    
-	    $badIDs = array();
-	    $type = $_REQUEST["type"];
-	
-	    if($type == "space"){
-		$URIbase = "subjects";
-	    }
-	    elseif($type == "media"){
-		$URIbase = "media";
-	    }
-	
-	
-	    if(!isset($_REQUEST["id"])){
-		$xmlURI = $_REQUEST["xmlURI"];
-		$xmlURI = 'http://ux.opencontext.org/XMLdocs/bad_bones.xml';
-		$xmlString = file_get_contents($xmlURI);
-		$xml = simplexml_load_string($xmlString);
-		
-		foreach($xml->xpath("//Bad_UUID") as $BadIdVal) {
-		    $BadId = $BadIdVal."";
-		    if(!in_array($BadId, $badIDs)){
-			$badIDs[] = $BadId;
-			echo "<br/>Bad item: <a href='../".$URIbase."/".$BadId."'>".$BadId."</a>";
-		    }
-		}
-	    }
-	    else{
-		$badIDs[] = $_REQUEST["id"];
-	    }
-	
-	
-	    if(!isset($_REQUEST["simple"])){
-		$doComplex = true;
-	    }
-	    else{
-		$doComplex = false;
-	    }
-	    
-	    if(($type == "space")&& $doComplex){
-		//get array of parent ids, so that their xml can be edited
-		$parentIDs = OpenContext_DeleteDocs::findParents($badIDs);
-		if(is_array($parentIDs)){
-		    foreach($parentIDs as $parentID){
-			
-			echo "<br/>Change Parent: <a href='../subjects/".$parentID."'>".$parentID."</a>";
-			$parentXML = file_get_contents($host."/subjects/".$parentID.".xml");
-			$originalXML = $parentXML;
-			
-			foreach($badIDs as $badChild){
-			    //remove children from the parent xml
-			    echo "<br/>Checking on: ".$badChild;
-			    $parentXML = OpenContext_DeleteDocs::childNodeRemove($badChild, $parentXML);
-			}
-			
-			if(md5($originalXML)!=md5($parentXML)){
-			    //if parent xml is different, save update
-			    $oldSaved = false;
-			    $oldSaved = OpenContext_DeleteDocs::saveBeforeUpdate($parentID, "space", $originalXML);
-			    
-			    if($oldSaved){
-				//if a record is saved of the old version, now go update the existing version
-				$data = OpenContext_NewDocs::spaceAdd($parentXML, true, true);
-				$indexUpdate = file_get_contents($host."/publish/itempublish?index=true");
-				//echo $indexUpdate;
-				
-				//header ("content-type: text/xml");
-				//echo $parentXML;  
-			    }
-			    
-			}
-			
-		    }//end loop
-		
-		}//end case of parent to edit
-	    }
-	
-	
-	    $solr = new Apache_Solr_Service('localhost', 8983, '/solr');
-		// test the connection to the solr server
-	    if (@$solr->ping()) {
-	    	
-		foreach($badIDs as $actBadID){
-		    if(($type == "space")&& $doComplex){
-			$childrenIDs = array();
-			$childrenIDs = OpenContext_DeleteDocs::getChildren($actBadID, $childrenIDs);
-			
-			foreach($childrenIDs as $badChild){
-			    //echo "<br/>".$badChild;
-			    echo "<br/>Bad Child: <a href='../subjects/".$badChild."'>".$badChild."</a>";
-			    OpenContext_DeleteDocs::spaceDeleteDB($badChild);
-			}
-			
-			try{
-			    $solr->deleteByMultipleIds($childrenIDs);
-			}
-			catch (Exception $e) {
-			    $output["error"] = "error deleting children items";
-			}
-			
-			unset($childrenIDs);
-			
-			OpenContext_DeleteDocs::spaceDeleteDB($actBadID);
-		    }//end case of space
-		    elseif($type == "media"){
-			OpenContext_DeleteDocs::mediaDeleteDB($actBadID);
-		    }
-
-		}//end loop through bad ids
-		
-		try{
-		    $solr->deleteByMultipleIds($badIDs);
-		    if(!isset($_REQUEST["id"])){
-			$solr->optimize();
-		    }
-		    else{
-			$solr->optimize();
-		    }
-		}
-		catch (Exception $e) {
-		    $output["error"] = "error deleting items";
-		}
-		
-	    }//end case of solr ping
-	    
-	    
-	}//end case with authorization
-    }//end function
-    
-    
-    */
-    
-    
-    
-    
+		  //echo $data->archaeoML;
+    }
    
 }
 
---
+++ application/models/ExportTable.php
@@ -0,0 +1,195 @@
+<?php
+
+
+//this class interacts with the database for accessing and changing table items
+class ExportTable {
+    
+	 public $tableID;
+	 public $title;
+	 public $updated;
+	 public $created;
+	 public $recordCount; //number of records in the total set
+	 public $fieldCount; //number of field counts
+	 public $metadata; //array for expression as JSON-LD
+	 
+	 public $db;
+	 
+	 
+	 function getByID($tableID){
+		  
+		  $tableID = $this->security_check($tableID);
+		  $db = $this->startDB();
+		  
+		  $sql = "SELECT * FROM export_tabs WHERE tableID = '$tableID' LIMIT 1; ";
+		  $result = $db->fetchAll($sql);
+		  if($result){
+				
+				$this->title = $result[0]["title"];
+				$this->tableID = $tableID;
+				$this->updated = $result[0]["updated"];
+				$this->created = $result[0]["created"];
+				$this->recordCount = $result[0]["recordCount"];
+				$this->fieldCount = $result[0]["fieldCount"];
+				$metadataJSON = $result[0]["metadata"];
+				$metadata = Zend_Json::decode($metadataJSON);
+				$this->metadata = $metadata;
+				return true;
+		  }
+		  else{
+				return false;
+		  }
+	 }
+	 
+	 
+	 function createUpdate($metadataString){
+		  $output = false;
+		  
+		  $valid = $this->readValidateMetadata($metadataString);
+		  unset($metadataString);
+		  if($valid){
+				
+				$output = true;
+				$db = $this->startDB();
+				$metadata = $this->metadata;
+				$tableID = $this->tableID;
+
+				$updatedDB = date("Y-m-d H:i:s", time());
+				$createdDB = $updatedDB;
+				
+				$data = array("title" => $this->title,
+								  "recordCount" => $this->recordCount,
+								  "updated" => $updatedDB,
+								  "recordCount" => $this->recordCount,
+								  "fieldCount" => $this->fieldCount
+								  );
+				
+				$recordExists = $this->getByID($tableID); //check to see if the record already exists
+				if($recordExists){
+					 
+					 $metadata["published"] = date("Y-m-d\TH:i:s\-07:00", strtotime($this->created)); //from the old record! Keep creation time same
+					 $metadata["updated"] = date("Y-m-d\TH:i:s\-07:00", strtotime($updatedDB));
+					 $data["metadata"] = Zend_Json::encode($metadata);
+					 
+					 $where = "tableID = '$tableID' ";
+					 
+					 try{
+						  $db->update("export_tabs", $data, $where);
+					 }
+					 catch (Exception $e)  {
+						  $output = array("error" => true, "message" => (string)$e );
+					 }
+				}
+				else{
+					 
+					 $metadata["published"] = date("Y-m-d\TH:i:s\-07:00", strtotime($createdDB));
+					 $metadata["updated"] = date("Y-m-d\TH:i:s\-07:00", strtotime($updatedDB));
+					 $data["tableID"] = $tableID;
+					 $data["created"] = $createdDB;
+					 $data["num_views"] = 0;
+					 $data["num_downloads"] = 0;
+					 $data["metadata"] = Zend_Json::encode($metadata);
+					 
+					 try{
+						  $db->insert("export_tabs", $data);
+					 }
+					 catch (Exception $e)  {
+						  $output = array("error" => true, "message" => (string)$e );
+					 }
+				}
+
+		  }
+		  else{
+				$output = array("error" => true, "message" => "JSON not valid");
+		  }
+		  return $output;
+	 }
+	 
+	 
+	 //reads and validates a JSON metadata string
+	 function readValidateMetadata($metadataString){
+		  
+		  $ouput = false;
+		  $metadata = Zend_Json::decode($metadataString);
+		  
+		  if(is_array($metadata)){
+				
+				$error = false;
+				if(!isset($metadata["@context"])){
+					 $error = true; //check if this is JSON-LD data
+				}
+				
+				if(isset($metadata["tableID"])){
+					 $this->tableID = $metadata["tableID"];
+				}
+				else{
+					 $error = true;
+				}
+				
+				if(isset($metadata["title"])){
+					 $this->title = $metadata["title"];
+				}
+				else{
+					 $error = true;
+				}
+				
+				
+				if(isset($metadata["recordCount"])){
+					 $this->recordCount = $metadata["recordCount"];
+				}
+				else{
+					 $error = true;
+				}
+				
+				if(isset($metadata["fieldCount"])){
+					 $this->fieldCount = $metadata["fieldCount"];
+				}
+				else{
+					 $error = true;
+				}
+				
+				if(!$error){
+					 $this->metadata = $metadata;
+					 $output = true;
+				}
+		  }
+
+		  return $output;
+	 }
+	 
+	 
+	 
+	 
+	 
+	 
+    function startDB(){
+		  if(!$this->db){
+				$db_params = OpenContext_OCConfig::get_db_config();
+				$db = new Zend_Db_Adapter_Pdo_Mysql($db_params);
+				$db->getConnection();
+				$this->setUTFconnection($db);
+				$this->db = $db;
+		  }
+		  else{
+				$db = $this->db;
+		  }
+		  return $db;
+	 }
+	 
+	 function setUTFconnection($db){
+		  $sql = "SET collation_connection = utf8_unicode_ci;";
+		  $db->query($sql, 2);
+		  $sql = "SET NAMES utf8;";
+		  $db->query($sql, 2);
+    }
+	 
+    function security_check($input){
+        $badArray = array("DROP", "SELECT", "#", "--", "DELETE", "INSERT", "UPDATE", "ALTER", "=");
+        foreach($badArray as $bad_word){
+            if(stristr($input, $bad_word) != false){
+                $input = str_ireplace($bad_word, "XXXXXX", $input);
+            }
+        }
+        return $input;
+    }
+    
+}
