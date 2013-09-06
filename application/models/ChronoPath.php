1fa08b12f785fe4becfc7e76713820142ea71d99
Eric Kansa <kansaeric@gmail.com>
2013-09-06 09:54:15 -0700

    Enhancement: Time-span Path Strings
    Added feature to add a prefix that indicates the maximum range of
    coverage of a chronology path. This can enable interoperability between
    paths of various maximum time ranges, since all information needed to
    decode a path is represented in the path itself.

--- application/models/ChronoPath.php
+++ application/models/ChronoPath.php
@@ -4,14 +4,19 @@
 //this class calculates a chronology path for making time-span facets
 class ChronoPath {
     
-    const maxDateBP = 5000000000; //5 Billion Year Ago
-	 const maxPathDepth = 34;
+    const defaultMaximumDateBP = 5000000000; //5 Billion Year Ago
+	 const maxPathDepth = 30;
+	 const minumumIntervalSpan = .5; //smallest size of a time interval that we care about
+	 
+	 const prefixDelim = "-";
 	 
 	 public $blockStart;
 	 public $blockEnd;
 	 public $blockMiddle;
+	 public $pathMaximiumBP;
 	 
 	 //Creates a chronology path as a string of numbers between 0-3, representing time spans for start and end dates BP
+	 //you can pass a path prefix like "10M-" to the $path to set the maxium BP for the current chronogy path
 	 function createPath($startDateBP, $endDateBP, $path = ""){
 		  
 		  if($startDateBP > $endDateBP){
@@ -20,39 +25,47 @@ class ChronoPath {
 		  else{
 				
 				$intervalSpan = $this->pathConvertBeginEnd($path);
-				$halfSpan = $intervalSpan/2;
-				//echo "<br/>".$intervalSpan." (".$this->blockStart." - ".$this->blockEnd."): $path";
-				if($startDateBP < $this->blockStart + $halfSpan){
-					 $Npath = "0";
-					 if($endDateBP >= $this->blockEnd - $halfSpan ){
-						  $Npath = "1";
+				if($intervalSpan > self::minumumIntervalSpan){
+					 $halfSpan = $intervalSpan/2;
+					 
+					 if($endDateBP > $this->pathMaximiumBP){
+						  //you've got a data range that doesn't fit into the current Maxiumum BP for this path
+						  return false;
+					 }
+					 
+					 if($startDateBP < $this->blockStart + $halfSpan){
+						  $Npath = "0";
+						  if($endDateBP >= $this->blockEnd - $halfSpan ){
+								$Npath = "1";
+						  }
+					 }
+					 else{
+						  $Npath = "2";
+						  if($endDateBP >= $this->blockEnd - $halfSpan ){
+								$Npath = "3";
+						  }
+					 }
+					 $path .= $Npath;
+					 
+					 if($this->getPathLevel($path) <= self::maxPathDepth){
+						  $path = $this->createPath($startDateBP, $endDateBP, $path);
 					 }
 				}
-				else{
-					 $Npath = "2";
-					 if($endDateBP >= $this->blockEnd - $halfSpan ){
-						  $Npath = "3";
-					 }
-				}
-				$path .= $Npath;
-				//echo "<br/>".$path;
-				if(strlen($path)< self::maxPathDepth){
-					 $path = $this->createPath($startDateBP, $endDateBP, $path);
-				}
-				
-				
 				return $path;
 		  }
 	 }//end function
     
 	 //decodes a chronology path and gets a start and end date for the range indicated by the path
-	 function pathConvertBeginEnd($path){
+	 function pathConvertBeginEnd($rawPath){
 
-		  $level = strlen($path);
-		  $this->blockStart = 0;
-		  $this->blockEnd = self::maxDateBP;
+		  $this->getPathMaximum($rawPath);
+		  $level = $this->getPathLevel($rawPath);
+		  $path = $this->trimPathPrefix($rawPath);
 		  
-		  $intervalSpan = self::maxDateBP;
+		  $this->blockStart = 0;
+		  $this->blockEnd = $this->pathMaximiumBP;
+		  
+		  $intervalSpan = $this->pathMaximiumBP;
 		  $i = 0;
 		  while($i < $level){
 		  
@@ -82,16 +95,58 @@ class ChronoPath {
 	 }
 	 
 	 
-	 function getLevelTimeLength($level){
-		  $i = 0;
-		  $span = self::maxDateBP;
-		  while($i < $level){
-				$span = $span / 2;
-				$i++;
+	 //checks if a prefix is on the path, if so, uses the prefix
+	 //to set the maximum time interval represented in the chronology path
+	 function getPathMaximum($rawPath){
+		  $doDefault = true;
+		  $expArray = array("k" => 3,
+								  "m" => 6,
+								  "g" => 9
+								  );
+		  
+		  if(strstr($rawPath, self::prefixDelim)){
+				$pathEx = explode(self::prefixDelim, $rawPath);
+				$prefix = $pathEx[0];
+				$prefix = strtolower($prefix);
+				$lastChar = preg_replace("/[^a-zA-Z]/", '', $prefix);
+				$numericPrefix = preg_replace("/[^0-9.]/", '', $prefix);
+				if(is_numeric($numericPrefix)){
+					 $doDefault = false;
+					 if(array_key_exists($lastChar, $expArray)){
+						  $this->pathMaximiumBP = $numericPrefix * (pow(10, $expArray[$lastChar]));
+					 }
+					 else{
+						  $this->pathMaximiumBP = $numericPrefix;
+					 }
+				}
 		  }
 		  
-		  return $span;
+		  if($doDefault){
+				$this->pathMaximiumBP = self::defaultMaximumDateBP;
+		  }
 	 }
 	 
+	 //get's the path's level, tacking into account a path prefix
+	 function getPathLevel($rawPath){
+		  $path = $this->trimPathPrefix($rawPath);
+		  return strlen($path);
+	 }
+	 
+	 //remove a prefix from a raw path
+	 function trimPathPrefix($rawPath){
+		  if(strstr($rawPath, self::prefixDelim)){
+				$pathEx = explode(self::prefixDelim, $rawPath);
+				if(isset($pathEx[1])){
+					 $path = $pathEx[1];
+				}
+				else{
+					 $path = "";
+				}
+				return $path;
+		  }
+		  else{
+				return $rawPath;
+		  }
+	 }
 	 
 }
